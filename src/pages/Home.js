265e72a6b743e874c1b589f2ebe643e8557ffff2
xTunnieTek <cnqtuannn@gmail.com>
2022-10-06 04:20:24 +0700

    Update 4:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
+import '../components/Hammer.js';
 import '../css/Home.css';
-
 
 const Home = () => {
     return (
@@ -9,19 +9,31 @@ const Home = () => {
             <Header />
             <Sidebar />
             <div class="Container">
+                <div class="Match-status">
+                    <h2 class="remove">Nope</h2>
+                    <h2 class="heart">Like</h2>
+                </div>
                 <div class="Heading">
-                    <h3>BTEC</h3>
                     <h3>Discover</h3>
+                    <p>BTEC</p>
                 </div>
-                <div class="Match">
-                    <div class="Match-Image">
-                        <img src="/assets/Images/Match/test.png" alt="Match" />
-                    </div> 
+                <div class="Matches">
+                    {/* 1 */}
+                    <div class="Match">
+                        <div class="Match-Image">
+                            <img src="/assets/Images/Match/test.png" alt="Match" />
+                            <div class="Blur"></div>
+                        </div> 
+                        <div class="Match-Info">
+                            <h3>Cuốc Hi, 20</h3>
+                            <h4>Software Engineer</h4>
+                        </div>
+                    </div>
                 </div>
-                <div class="Controll">
-                    <button id="nope"><img src="/assets/SVG/Undo.svg" alt="Love" /></button>
-                    <button id="love"><img src="/assets/SVG/Love100.svg" alt="Love" /></button>
-                    <button id="love"><img src="/assets/SVG/Star.svg" alt="Love" /></button>
+                <div class="Control">
+                    <button id="undo"><img src="/assets/Icon/Dislike.png"/></button>
+                    <button id="love"><img src="/assets/Icon/Like.png" /></button>
+                    <button id="star"><img src="/assets/Icon/Star.png"/></button>
                 </div>
             </div>
         </div>
